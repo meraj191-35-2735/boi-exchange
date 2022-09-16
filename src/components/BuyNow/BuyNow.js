1c1c4f386f31b500c23be8580fc6ef3e87b384e2
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-09-16 18:05:22 +0600

    buy now page done

--- src/components/BuyNow/BuyNow.js
+++ src/components/BuyNow/BuyNow.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 // import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
-import auth from "../../firebase.init";
+// import auth from "../../firebase.init";
 
 const BuyNow = () => {
   //   const [user] = useAuthState(auth);
