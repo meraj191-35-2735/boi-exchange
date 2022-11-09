2d80458396e93dab2128b955a13d9316d86818b0
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-11-09 20:11:24 +0600

    problem fixed

--- src/components/ExchangePage/ExchangePage.js
+++ src/components/ExchangePage/ExchangePage.js
@@ -1,32 +1,58 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 import Footer from "../Footer/Footer";
 import Loading from "../Loading/Loading";
 import Books from "./Books";
 
 const ExchangePage = () => {
-  const [books, setBooks] = useState([]);
+  const [user] = useAuthState(auth);
+  const [booksMine, setBooks] = useState([]);
   useEffect(() => {
     fetch("https://floating-gorge-66618.herokuapp.com/exchange")
       .then((res) => res.json())
       .then((data) => setBooks(data));
   }, []);
-  if (books.length === 0) {
+  const books = [];
+  for (const book of booksMine) {
+    if (book.userEmail !== user?.email) {
+      books.push(book);
+    }
+  }
+
+  if (booksMine.length === 0) {
     return <Loading></Loading>;
   }
   return (
-    <div>
-      <h1 className="text-center font-serif font-bold text-green-600 text-3xl py-5">
-        Books Available For Exchange
-      </h1>
-      <div className=" w-full mx-auto">
-        {books.map((book) => (
-          <Books key={books.indexOf(book)} book={book}></Books>
-        ))}
-      </div>
-      <Footer></Footer>
-    </div>
+    <>
+      {user ? (
+        <div>
+          <h1 className="text-center font-serif font-bold text-green-600 text-3xl py-5">
+            Books Available For Exchange
+          </h1>
+          <div className=" w-full mx-auto">
+            {books.map((book) => (
+              <Books key={books.indexOf(book)} book={book}></Books>
+            ))}
+          </div>
+          <Footer></Footer>
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-center font-serif font-bold text-green-600 text-3xl py-5">
+            Books Available For Exchange
+          </h1>
+          <div className=" w-full mx-auto">
+            {booksMine.map((book) => (
+              <Books key={books.indexOf(book)} book={book}></Books>
+            ))}
+          </div>
+          <Footer></Footer>
+        </div>
+      )}
+    </>
   );
 };
 
